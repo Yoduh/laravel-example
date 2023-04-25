494d4c74f0931287569d69b33874d0c9a39ae803
Yoduh <ahandlovits@gmail.com>
2023-04-24 23:05:00 -0400

    initial

---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>Show</div>
+    <Link href="/">Index Page</Link>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<style lang="scss" scoped></style>
